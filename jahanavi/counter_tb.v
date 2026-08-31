44fd983579ea22eda1fcf68c19708c990f20d8d7
R C Jahanavi <rcjahanavi.23ec@saividya.ac.in@svit.edu.in>
2026-08-31 09:37:44 +0000

    counter

---
+++ jahanavi/counter_tb.v
@@ -0,0 +1,24 @@
+module counter_tb;
+reg clk;
+reg reset;
+wire [3:0] count;
+
+counter c1(
+.clk(clk),
+.reset(reset),
+.count(count)
+);
+
+initial begin
+$dumpfile("counter.vcd");
+$dumpvars(0, counter_tb);
+
+clk =0;
+reset =1;
+#10;
+reset =0;
+#100;
+$finish;
+end
+always #5 clk = ~clk;
+endmodule
